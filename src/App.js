349335b4937bc06bc8880e5e6c89aa19f573404b
Harsh <99734334+harsh-9389@users.noreply.github.com>
2024-10-18 20:00:16 +0530

    Header and search input created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-b from-blue-500 to-blue-100 h-fit shadow-xl shadow-gray-400 rounded-2xl'>
+      <TopButtons/>
+      <Inputs/>
     </div>
   );
 }
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function TopButtons() {
+  // (Delhi, Mumbai, Chennai, Bangalore, Kolkata, Hyderabad)
+  const cities = [
+    {
+      id: 1,
+      name: 'Delhi'
+    },
+    {
+      id: 2,
+      name: 'Mumbai'
+    },
+    {
+      id: 3,
+      name: 'Chennai'
+    },
+    {
+      id: 4,
+      name: 'Bangalore'
+    },
+    {
+      id: 5,
+      name: 'Kolkata'
+    },
+    {
+      id: 6,
+      name: 'Hyderabad'
+    }
+  ] ;
+
+  return(
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">{city.name}</button>
+      ))}
+    </div>
+  )
+}
+
+export default TopButtons;
