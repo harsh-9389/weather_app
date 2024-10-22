8fe343135e4fbce4b1d584d203c9926375eb255b
Harsh <99734334+harsh-9389@users.noreply.github.com>
2024-10-23 02:42:36 +0530

    completed

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -14,22 +14,6 @@ function Inputs({ setQuery, units, setUnits }) {
     if (city !== "") setQuery({ q: city });
   };
 
-  const handleLocationClick = () => {
-    if (navigator.geolocation) {
-      toast.info("Fetching users location.");
-      navigator.geolocation.getCurrentPosition((position) => {
-        toast.success("Location fetched!");
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-
-        setQuery({
-          lat,
-          lon,
-        });
-      });
-    }
-  };
-
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
@@ -38,17 +22,12 @@ function Inputs({ setQuery, units, setUnits }) {
           onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search for city...."
-          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase rounded-2xl"
         />
         <UilSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
           onClick={handleSearchClick}
-        />
-        <UilLocationPoint
-          size={25}
-          className="text-white cursor-pointer transition ease-out hover:scale-125"
-          onClick={handleLocationClick}
         />
       </div>
 
